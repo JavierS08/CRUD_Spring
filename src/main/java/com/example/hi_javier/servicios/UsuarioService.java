348addcc562a36cd72f0db15d4ec23b9f38a75af
Javier Sánchez <javier.sanchezc@campusfp.es>
2023-02-17 14:04:33 +0100

    penultimo commit

--- src/main/java/com/example/hi_javier/servicios/UsuarioService.java
+++ src/main/java/com/example/hi_javier/servicios/UsuarioService.java
@@ -1,6 +1,4 @@
 package com.example.hi_javier.servicios;
-
-
 import com.example.hi_javier.jpa.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,7 +6,6 @@ import org.springframework.web.context.annotation.ApplicationScope;
 
 import java.util.List;
 import java.util.Optional;
-
 @Service
 @ApplicationScope
 public class UsuarioService {
@@ -17,21 +14,15 @@ public class UsuarioService {
     public UsuarioService(UsuarioRepositorio usuarios) {
         this.usuarios = usuarios;
     }
-
-
     public List<Usuario> listaUsuarios() {
         return usuarios.findAll();
     }
-
-
     public Optional<Usuario> buscarUsuario(String login) {
         return usuarios.findById(login);
     }
-
     public void borrar(Usuario u){
         usuarios.delete(u);
     }
-
     public void guardarUsuario(Usuario user) {
         usuarios.save(user);
     }
